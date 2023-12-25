d180697890a57e77d271c44ca89588d5a8bdc77d
Loy Yee Ko <koloyyee@gmail.com>
2023-12-25 12:24:08 -0500

    commit part2

--- part12-Part12_06.Numbers/src/main/java/Program.java
+++ part12-Part12_06.Numbers/src/main/java/Program.java
@@ -6,6 +6,12 @@ public class Program {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("How many random numbers should be printed?");
+        int input = Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i < input; i++) {
+            int rand = new Random().nextInt(11);
+            System.out.println(rand);
+        }
     }
 
 }
--- part12-Part12_08.Lottery/src/main/java/LotteryRow.java
+++ part12-Part12_08.Lottery/src/main/java/LotteryRow.java
@@ -20,11 +20,24 @@ public class LotteryRow {
         this.numbers = new ArrayList<>();
         // Implement the random number generation here
         // the method containsNumber is probably useful
+        int hi = 40;
+        int lo = 1;
+
+        while (this.numbers.size() < 7) {
+            int rand = new Random().nextInt(hi) + lo;
+            if (!containsNumber(rand)) {
+                numbers.add(rand);
+            }
+        }
     }
 
     public boolean containsNumber(int number) {
         // Check here whether the number is among the drawn numbers
+        for (int num : numbers) {
+            if (number == num) {
+                return true;
+            }
+        }
         return false;
     }
 }
-
